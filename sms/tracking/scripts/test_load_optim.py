b7b6a8e3c75286182ea9914f1e1744a45fa53582
Justin Yu <yujustin8@gmail.com>
2024-07-11 12:31:44 -0700

    tracking testing

---
+++ sms/tracking/scripts/test_load_optim.py
@@ -0,0 +1,26 @@
+from sms.tracking.optim import Optimizer
+from pathlib import Path
+import torch
+import viser.transforms as vtf
+import numpy as np
+
+CKPT = '/home/yujustin/Desktop/sms/sms/data/utils/Detic/outputs/stapler_apple_scissor/sms-data/2024-07-11_112702/config.yml'
+def main(
+    config_path: Path = Path(CKPT),
+    ):
+    opt = Optimizer(
+                config_path,
+                # zed.get_K(),
+                # l.shape[1],
+                # l.shape[0], 
+                # # zed.width,
+                # # zed.height,
+                # init_cam_pose=torch.from_numpy(
+                #     vtf.SE3(
+                #         wxyz_xyz=np.array([*camera_frame.wxyz, *camera_frame.position])
+                #     ).as_matrix()[None, :3, :]
+                # ).float(),
+            )
+
+if __name__ == '__main__':
+    main()
