3be97d87c3e8a85da7f7aed67f8933fea4379fb4
Justin Yu <yujustin8@gmail.com>
2024-07-21 01:20:30 -0700

    instance dims to 128

--- sms/fields/gaussian_field.py
+++ sms/fields/gaussian_field.py
@@ -99,7 +99,7 @@ class GaussianField(Field):
             ]
         )
         tot_out_dims = sum([e.n_output_dims for e in self.clip_encs])
-        instance_n_dims = 64
+        instance_n_dims = 128
         print("Total output dims: ", tot_out_dims)
 
         # self.mlp_base_grid = HashEncoding(
